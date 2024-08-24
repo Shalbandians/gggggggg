bdaa42c8a30ad9ff1c26ee55cbc8dbc683d10ea9
Muhammad Rizwan <raziramzan001@gmail.com>
2024-08-24 12:37:40 +0500

    frontend updated url add done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors"); 
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://multi-vender-b42z.vercel.app/',
   credentials: true
 }));
 
